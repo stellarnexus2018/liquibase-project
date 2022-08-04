fa047cdfc47e32c00c04d815160522958a3793ec
Konstantin Smirnov <covox@covox.ru>
2022-08-04 13:39:40 +0300

    Some helpers

--- src/main/java/ru/sberinsur/insure/listeners/SpecificListener.java
+++ src/main/java/ru/sberinsur/insure/listeners/SpecificListener.java
@@ -5,20 +5,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.Message;
+import org.springframework.validation.annotation.Validated;
+import ru.sberinsur.insure.integrations.commons.Message;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Service;
 import ru.sberinsur.insure.config.KafkaConfig;
 import ru.sberinsur.insure.exception.TemplateErrors;
+import ru.sberinsur.insure.integrations.commons.KafkaHeaderAccessor;
 import ru.sberinsur.insure.integrations.commons.KafkaHelper;
 import ru.sberinsur.insure.integrations.commons.InsureListener;
 import ru.sberinsur.insure.integrations.dto.template.ReponseTemplateDto;
 import ru.sberinsur.insure.integrations.dto.template.RequestTemplateDto;
+import ru.sberinsur.insure.integrations.exception.InsureErrorCode;
 import ru.sberinsur.insure.integrations.exception.InsureException;
 
+
 import java.util.Map;
+import java.util.UUID;
 
 
 /**
@@ -28,6 +33,7 @@ import java.util.Map;
 @KafkaListener(containerFactory = "specificKafkaListenerContainerFactory",
         topics = {"${kafka.specificConsumer.topic}"})
 @Slf4j
+@Validated
 @RequiredArgsConstructor
 public class SpecificListener extends InsureListener {
 
@@ -37,7 +43,7 @@ public class SpecificListener extends InsureListener {
 
     @KafkaHandler
     @SendTo("!{source.headers['kafka_replyTopic']}")
-    public Message<ReponseTemplateDto> getCustomer(RequestTemplateDto requestTemplateDto, @Headers Map<String, Object> headers, Acknowledgment ack) {
+    public org.springframework.messaging.Message<ReponseTemplateDto> getCustomer(RequestTemplateDto requestTemplateDto, @Headers Map<String, Object> headers, Acknowledgment ack) {
 
         try {
             log.info("Received specific topic {}", requestTemplateDto);
@@ -61,5 +67,30 @@ public class SpecificListener extends InsureListener {
 
     }
 
+    private void processChainResponseMessage(Map<String, Object> headers, ru.sberinsur.insure.integrations.commons.Message message, InsureErrorCode insureErrorCode, Acknowledgment ack) {
+
+        KafkaHeaderAccessor kafkaHeaderAccessor = KafkaHeaderAccessor.ofMap(headers);
+        if (null == kafkaHeaderAccessor.chainRequestId()) {
+            throw new InsureException("INSURE_CHAIN_RQ_ID is null!");
+        }
+
+        try {
+            if (kafkaHeaderAccessor.destinationInstanceId().equalsIgnoreCase(this.defaultKafkaConfig.getSpecificConsumer().getGroupId())) {
+                log.info("Received message specific topic {}", message.toString());
+
+                if (null != headers && !headers.isEmpty()) {
+                    this.kafkaHelper.defaultPrintNotNullKafkaHeaders(headers);
+                }
+
+                UUID messageId = kafkaHeaderAccessor.correlationId();
+                UUID rquid = kafkaHeaderAccessor.chainRequestId();
+                log.debug("Rquid: {}; UUID: {}", rquid, messageId.toString());
+                this.waiter.updateResponseHolder(rquid, messageId, message, headers);
+            }
+            ack.acknowledge();
+        } catch (Exception e) {
+            throw new InsureException(e.getMessage(), e, ((e instanceof InsureException) ? ((InsureException) e).getInsureErrorCode() : insureErrorCode));
+        }
+    }
 
 }
---
+++ src/main/java/ru/sberinsur/insure/service/callbacks/CallbacksHelper.java
@@ -0,0 +1,49 @@
+package ru.sberinsur.insure.service.callbacks;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.stereotype.Component;
+import ru.sberinsur.insure.integrations.commons.Headers;
+import ru.sberinsur.insure.integrations.commons.KafkaHeaderAccessor;
+import ru.sberinsur.insure.integrations.commons.Message;
+import org.springframework.kafka.core.KafkaTemplate;
+import java.util.Map;
+import java.util.UUID;
+import ru.sberinsur.insure.integrations.commons.KafkaHelper;
+
+import static ru.sberinsur.insure.integrations.commons.Headers.INSURE_CHAIN_RQ_ID;
+import static ru.sberinsur.insure.integrations.commons.KafkaHelper.bytesToUUID;
+
+@Component
+@RequiredArgsConstructor
+public class CallbacksHelper {
+
+    private final KafkaTemplate<String, Message> kafkaTemplate;
+
+    /**
+     * Send chain result dto. Equal outDtoType parameter from @ChainHandler
+     * <p>
+     * For example: @ChainHandler(outDtoType = SearchApplicationByFiltersResult.class)
+     *
+     * @param headers
+     * @param message
+     */
+    public void sendChainResult(Map<String, Object> headers, Message message) {
+        KafkaHeaderAccessor kafkaHeaderAccessor = KafkaHeaderAccessor.ofMap(headers);
+        ProducerRecord producerRecord = new ProducerRecord<>(kafkaHeaderAccessor.topic(), null, UUID.randomUUID().toString(), message, kafkaHeaderAccessor.toKafkaHeaders());
+        this.kafkaTemplate.send(producerRecord);
+    }
+
+    /**
+     * Check and fetch INSURE_CHAIN_RQ_ID
+     *
+     * @param rootHeaders
+     * @return
+     */
+    public UUID getChainRq(Map<String, Object> rootHeaders) {
+        if (null == rootHeaders.get(INSURE_CHAIN_RQ_ID.name()))
+            throw new IllegalArgumentException(String.format("Empty %s header", Headers.INSURE_CHAIN_RQ_ID.name()));
+        return bytesToUUID((byte[]) rootHeaders.get(INSURE_CHAIN_RQ_ID.name()));
+    }
+
+}

42800937cc39a81b1688c2134ed07d4e19fbf754
Ilya Gutorov <igutorov@sberinsur.com>
2023-07-12 00:34:40 +0300

    add bpm request handler to GroupListener

--- src/main/java/ru/sberinsur/insure/listeners/GroupListener.java
+++ src/main/java/ru/sberinsur/insure/listeners/GroupListener.java
@@ -10,15 +10,19 @@ import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Service;
-import ru.sberinsur.insure.config.KafkaConfig;
 import ru.sberinsur.insure.exception.TemplateErrors;
 import ru.sberinsur.insure.integrations.commons.InsureListener;
+import ru.sberinsur.insure.integrations.dto.bpm.common.ErrorDto;
+import ru.sberinsur.insure.integrations.dto.bpm.externalTask.CompleteExternalTaskDto;
+import ru.sberinsur.insure.integrations.dto.bpm.externalTask.ExecuteExternalTaskDto;
 import ru.sberinsur.insure.integrations.dto.common.doc.DocIgnore;
 import ru.sberinsur.insure.integrations.dto.template.ReponseTemplateDto;
 import ru.sberinsur.insure.integrations.dto.template.RequestTemplateDto;
 import ru.sberinsur.insure.integrations.exception.InsureException;
+import ru.sberinsur.insure.integrations.exception.SystemErrorCode;
 import ru.sberinsur.insure.util.MapperUtil;
 
+import java.util.Arrays;
 import java.util.Map;
 
 
@@ -31,8 +35,6 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 public class GroupListener extends InsureListener {
-
-    private final KafkaConfig kafkaConfig;
 
     @KafkaHandler
     @SendTo("!{source.headers['kafka_replyTopic']}")
@@ -54,6 +56,40 @@ public class GroupListener extends InsureListener {
         }
     }
 
+    @KafkaHandler
+    @SendTo("!{source.headers['kafka_replyTopic']}")
+    public Message<CompleteExternalTaskDto> bpmRequest(ExecuteExternalTaskDto request, @Headers Map<String, Object> headers, Acknowledgment ack) {
+        log.info("[bpmRequest] Received request");
+        log.info("[bpmRequest] Request headers: {}", headers);
+        try {
+            log.trace("[bpmRequest] Received group topic {}", MapperUtil.toJsonString(request));
+            if (null != headers && !headers.isEmpty()) {
+                this.kafkaHelper.defaultPrintNotNullKafkaHeaders(headers);
+            }
+            var completeExternalTaskDto = processingBpmRequest(request, headers);
+            ack.acknowledge();
+            var outgoingHeaders = kafkaHelper.correlateHeaders(headers,
+                    defaultKafkaConfig.getSpecificConsumer().getGroupId());
+            return new GenericMessage<>(completeExternalTaskDto, outgoingHeaders);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new InsureException(e.getMessage(), e, SystemErrorCode.INTERNAL_ERROR);
+        }
+    }
+
+    private CompleteExternalTaskDto processingBpmRequest(ExecuteExternalTaskDto request, Map<String, Object> headers) {
+        try {
+            // business logic call here
+            Map<String, Object> returnVariables = Map.of();
+
+            return new CompleteExternalTaskDto(request.getTask(), returnVariables, null);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            String businessError = (e instanceof InsureException) ?
+                    ((InsureException) e).getInsureErrorCode().getErrorDescription() : null;
+            return new CompleteExternalTaskDto(request.getTask(), Map.of(), new ErrorDto(e.getMessage(), Arrays.toString(e.getStackTrace()), businessError));
+        }
+    }
 
     @KafkaHandler
     @DocIgnore
